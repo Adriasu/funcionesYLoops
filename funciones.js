4f25557e8ac99a5e151c6d5f04200caae499ad96
Adriasu <adriana.suarez@students.gammatech.school>
2024-04-30 23:50:04 +0200

    ejercicio 3 termiando

--- funciones.js
+++ funciones.js
@@ -1,20 +1,27 @@
-// EJERCICIO 1
+// // EJERCICIO 1
 
-const side1 = parseInt(prompt("Ingrese la medida del lado 1 del rectángulo"));
-const side2 = parseInt(prompt("Ingrese la medida del lado 2 del rectángulo"));
+// const side1 = parseInt(prompt("Ingrese la medida del lado 1 del rectángulo"));
+// const side2 = parseInt(prompt("Ingrese la medida del lado 2 del rectángulo"));
 
-function rectangleArea(a = 5, b = 3) {
-  return a * b;
+// function rectangleArea(a = 5, b = 3) {
+//   return a * b;
+// }
+// console.log(rectangleArea(side1, side2));
+// console.log(rectangleArea());
+
+// // EJERCICIO 2
+
+// function celsiusToFahrenheit(c) {
+//   return (c * 9) / 5 + 32;
+// }
+// console.log(celsiusToFahrenheit(25));
+
+// EJERCICIO 3
+
+function randomNumber(min, max) {
+  return parseInt(Math.random() * (max - min + 1) + min);
 }
-console.log(rectangleArea(side1, side2));
-console.log(rectangleArea());
-
-// EJERCICIO 2
-
-function celsiusToFahrenheit(c) {
-  return (c * 9) / 5 + 32;
-}
-console.log(celsiusToFahrenheit(25));
+console.log(randomNumber(5, 20));
 
 
 
