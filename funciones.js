172e406325e72abdc8559d0a4cf23659ab7e7bf5
Adriasu <adriana.suarez@students.gammatech.school>
2024-04-30 22:53:01 +0200

    ejercicio 1 terminado

--- funciones.js
+++ funciones.js
@@ -1,10 +1,15 @@
-// EJERCICIO 1 
+// EJERCICIO 1
 
-const side1 = parseInt(prompt("Ingrese la medida del lado 1 del rectángulo"))
-const side2 = parseInt(prompt("Ingrese la medida del lado 2 del rectángulo"))
+const side1 = parseInt(prompt("Ingrese la medida del lado 1 del rectángulo"));
+const side2 = parseInt(prompt("Ingrese la medida del lado 2 del rectángulo"));
 
-function rectangleArea(a,b) {
-    
+function rectangleArea(a = 5, b = 3) {
+    return a * b
 }
+console.log(rectangleArea(side1, side2));   
+console.log(rectangleArea());
+
+
+
 
 
