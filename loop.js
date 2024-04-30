4e15167872094c215eecbe3abf3c9abe6d042b72
Adriasu <adriana.suarez@students.gammatech.school>
2024-05-01 00:51:57 +0200

    ejercicio 1 bucles terminado

---
+++ loop.js
@@ -0,0 +1,22 @@
+// // EJERCICIO 1
+
+// for (let i = 1; i <= 10; i++) {
+//   console.log(i);
+// }
+
+// let counter = 1;
+// while (counter <= 10) {
+//   console.log(counter);
+//   counter++;
+// }
+
+// for (let i = 83; i >= 24; i--) {
+//   console.log(i);
+// }
+
+// let uncounter = 83;
+// while (uncounter >= 24) {
+//   console.log(uncounter);
+//   uncounter--;
+// }
+
