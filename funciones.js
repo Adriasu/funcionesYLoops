8ff51abafb6498a46bf02d72be780ba61f7bd658
Adriasu <adriana.suarez@students.gammatech.school>
2024-04-30 23:07:26 +0200

    ejercicio 2 terminado

--- funciones.js
+++ funciones.js
@@ -4,12 +4,17 @@ const side1 = parseInt(prompt("Ingrese la medida del lado 1 del rectángulo"));
 const side2 = parseInt(prompt("Ingrese la medida del lado 2 del rectángulo"));
 
 function rectangleArea(a = 5, b = 3) {
-    return a * b
+  return a * b;
 }
-console.log(rectangleArea(side1, side2));   
+console.log(rectangleArea(side1, side2));
 console.log(rectangleArea());
 
+// EJERCICIO 2
 
+function celsiusToFahrenheit(c) {
+  return (c * 9) / 5 + 32;
+}
+console.log(celsiusToFahrenheit(25));
 
 
 
