993a4a83e11b2bd645b07d3061e3f3b397d2e6c5
Adriasu <adriana.suarez@students.gammatech.school>
2024-05-01 20:56:12 +0200

    añadir otra forma del ejercicio 7 loop

--- funciones.js
+++ funciones.js
@@ -1,46 +1,46 @@
-// // EJERCICIO 1
+// EJERCICIO 1
 
-// const side1 = parseInt(prompt("Ingrese la medida del lado 1 del rectángulo"));
-// const side2 = parseInt(prompt("Ingrese la medida del lado 2 del rectángulo"));
+const side1 = parseInt(prompt("Ingrese la medida del lado 1 del rectángulo"));
+const side2 = parseInt(prompt("Ingrese la medida del lado 2 del rectángulo"));
 
-// function rectangleArea(a = 5, b = 3) {
-//   return a * b;
-// }
-// console.log(rectangleArea(side1, side2));
-// console.log(rectangleArea());
+function rectangleArea(a = 5, b = 3) {
+  return a * b;
+}
+console.log(rectangleArea(side1, side2));
+console.log(rectangleArea());
 
-// // EJERCICIO 2
+// EJERCICIO 2
 
-// function celsiusToFahrenheit(c) {
-//   return (c * 9) / 5 + 32;
-// }
-// console.log(celsiusToFahrenheit(25));
+function celsiusToFahrenheit(c) {
+  return (c * 9) / 5 + 32;
+}
+console.log(celsiusToFahrenheit(25));
 
-// // EJERCICIO 3
+// EJERCICIO 3
 
-// function randomNumber(min, max) {
-//   return parseInt(Math.random() * (max - min + 1) + min);
-// }
-// console.log(randomNumber(5, 20));
+function randomNumber(min, max) {
+  return parseInt(Math.random() * (max - min + 1) + min);
+}
+console.log(randomNumber(5, 20));
 
-// // EJERCICIO 4
+// EJERCICIO 4
 
-// const circleRadius = parseInt(prompt("Ingresa el radio del círculo"));
+const circleRadius = parseInt(prompt("Ingresa el radio del círculo"));
 
-// function circleArea(r) {
-//   return Math.PI * r ** 2;
-// }
-// console.log(circleArea(circleRadius));
+function circleArea(r) {
+  return Math.PI * r ** 2;
+}
+console.log(circleArea(circleRadius));
 
-// // EJERCICIO 5
+// EJERCICIO 5
 
-// const city = prompt("Ingrese una ciudad");
-// const job = prompt("ingrese un trabajo");
-// const sons = prompt("ingrese número de hijos que quiere tener");
-// const couple = prompt("ingrese el nombre de su pareja");
+const city = prompt("Ingrese una ciudad");
+const job = prompt("ingrese un trabajo");
+const sons = prompt("ingrese número de hijos que quiere tener");
+const couple = prompt("ingrese el nombre de su pareja");
 
-// function fortuneTeller(a, b, c, d) {
-//   return `Trabajaras en ${a}, trabajando como ${b}, y tendrás ${c} hijos con ${d}.`;
-// }
+function fortuneTeller(a, b, c, d) {
+  return `Trabajaras en ${a}, trabajando como ${b}, y tendrás ${c} hijos con ${d}.`;
+}
 
-// console.log(fortuneTeller(city, job, sons, couple));
+console.log(fortuneTeller(city, job, sons, couple));
--- loop.js
+++ loop.js
@@ -1,87 +1,92 @@
-// // EJERCICIO 1
+// EJERCICIO 1
 
-// for (let i = 1; i <= 10; i++) {
-//   console.log(i);
-// }
+for (let i = 1; i <= 10; i++) {
+  console.log(i);
+}
 
-// let counter = 1;
-// while (counter <= 10) {
-//   console.log(counter);
-//   counter++;
-// }
+let counter = 1;
+while (counter <= 10) {
+  console.log(counter);
+  counter++;
+}
 
-// for (let i = 83; i >= 24; i--) {
-//   console.log(i);
-// }
+for (let i = 83; i >= 24; i--) {
+  console.log(i);
+}
 
-// let uncounter = 83;
-// while (uncounter >= 24) {
-//   console.log(uncounter);
-//   uncounter--;
-// }
+let uncounter = 83;
+while (uncounter >= 24) {
+  console.log(uncounter);
+  uncounter--;
+}
 
-// // EJERCICIO 2
+// EJERCICIO 2
 
-// let num = 5;
+let num = 5;
 
-// for (let i = 0; i <= 10; i++) {
-//   console.log(`${num} x ${i} = ${num * i}`);
-// }
+for (let i = 0; i <= 10; i++) {
+  console.log(`${num} x ${i} = ${num * i}`);
+}
 
-// // EJERCICIO 3
+// EJERCICIO 3
 
-// for (let i = 0; i <= 10; i++) {
-//     console.log(i, i**2, i**3);
-// }
+for (let i = 0; i <= 10; i++) {
+    console.log(i, i**2, i**3);
+}
 
-// // EJERCICIO 4
+// EJERCICIO 4
 
-// let sum = 1;
-// for (let i = 0; i <= 10; i++) {
-//   sum += i;
-// }
-// console.log(sum);
+let sum = 1;
+for (let i = 0; i <= 10; i++) {
+  sum += i;
+}
+console.log(sum);
 
-// // EJERCICIO 5
+// EJERCICIO 5
 
-// for (let i = 0; i <= 100; i++) {
-//   if (i >= 10 && i <= 23) {
-//     console.log("del 10 al 23", i);
-//   }
-//   if (i >= 56 && i <= 62) {
-//     console.log("del 56 al 62", i);
-//   }
-//   if (i % 5 === 0 || i % 7 === 0) {
-//     console.log("divisibles entre 5 o 7", i);
-//   }
-// }
+for (let i = 0; i <= 100; i++) {
+  if (i >= 10 && i <= 23) {
+    console.log("del 10 al 23", i);
+  }
+  if (i >= 56 && i <= 62) {
+    console.log("del 56 al 62", i);
+  }
+  if (i % 5 === 0 || i % 7 === 0) {
+    console.log("divisibles entre 5 o 7", i);
+  }
+}
 
-// // EJERCICIO 6
+// EJERCICIO 6
 
-// const finalNumber = prompt("Ingrese un número");
+const finalNumber = prompt("Ingrese un número");
 
-// let evenNumber = 0;
-// let oddNumber = 0;
+let evenNumber = 0;
+let oddNumber = 0;
 
-// for (let i = 0; i <= finalNumber; i++) {
-//   if (i % 2 === 0) {
-//     evenNumber += i;
-//   } else {
-//     oddNumber += i;
-//   }
-// }
+for (let i = 0; i <= finalNumber; i++) {
+  if (i % 2 === 0) {
+    evenNumber += i;
+  } else {
+    oddNumber += i;
+  }
+}
 
-// console.log(
-//   `La suma de todos los números pares del 0 al ${finalNumber} es ${evenNumber}, y la suma de todos los números impares del 0 al ${finalNumber} es ${oddNumber}.`
-// );
+console.log(
+  `La suma de todos los números pares del 0 al ${finalNumber} es ${evenNumber}, y la suma de todos los números impares del 0 al ${finalNumber} es ${oddNumber}.`
+);
 
-//console.log(evenNumber, oddNumber);
+console.log(evenNumber, oddNumber);
 
-// // EJERCICIO 7
+// EJERCICIO 7
 
-// for (let i = "#"; i.length <= 7; i += "#"){
-//     console.log(i);
-//     }
+for (let i = "#"; i.length <= 7; i += "#"){
+    console.log(i);
+    }
+
+let print = ""
+for (let i = 1; i <= 7; i++){
+    console.log (print += "#");
+}
 
 // EJERCICIO 8
 
@@ -104,11 +109,11 @@ for (let i = 12; i >= 2; i -= 2) {
   for (let j = 0; j < space; j++) {
     character += " ";
   }
-  space++
-  for (let j = 0; j < i; j++){
-    character += "#"
+  space++;
+  for (let j = 0; j < i; j++) {
+    character += "#";
   }
-  character += "\n"
+  character += "\n";
 }
 
 console.log(character);
